6ec15e7969d07c3b3418e0085ba1bd4b3db232b3
Wang Xizhuo <71929217+WangXizhuo@users.noreply.github.com>
2022-11-16 22:39:13 +0800

    Update aggre_feature.py

--- aggre_feature.py
+++ aggre_feature.py
@@ -125,7 +125,7 @@ def aggregate_features(X_train,mask=False,time=False):
         else:
             pass
         if mask == True:
-           df[f'{feature}_mask'] = X_train[feature,'mask'].mean(axis=1) 
+           df[f'{feature}_mask'] = X_train[feature,'mask'].sum(axis=1) 
         if time == True:
             df[f'{feature}_time'] = X_train[feature,'time_since_measured'].mean(axis=1) 
         df[f'{feature}_range'] = X_train[feature,'mean'].max(axis=1) - X_train[feature,'mean'].min(axis=1)
